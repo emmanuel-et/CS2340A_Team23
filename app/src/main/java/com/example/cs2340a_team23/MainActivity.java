a1d2dbcd9151f192742654c39bb0d61f00a75059
simisola oshikanlu <immanuella2004@gmail.com>
2023-09-16 17:19:43 -0400

    Updated MainActivity.java and layout files

--- app/src/main/java/com/example/cs2340a_team23/MainActivity.java
+++ app/src/main/java/com/example/cs2340a_team23/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.cs2340a_team23;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button startButton;
+    private Button quitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
+        quitButton = findViewById(R.id.quitButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startGame();
+            }
+        });
+
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                quitGame();
+            }
+        });
+
+    }
+    public void startGame() {
+        //
+    }
+    public void quitGame() {
+        //
     }
 }
