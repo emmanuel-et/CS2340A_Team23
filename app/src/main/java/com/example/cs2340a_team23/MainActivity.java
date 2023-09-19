e83a629d9d446948585f058e1178546b4c9399f4
simisola oshikanlu <immanuella2004@gmail.com>
2023-09-18 22:13:59 -0400

    organized code based on checkstyle

--- app/src/main/java/com/example/cs2340a_team23/MainActivity.java
+++ app/src/main/java/com/example/cs2340a_team23/MainActivity.java
@@ -15,25 +15,13 @@ public class MainActivity extends AppCompatActivity {
         Button quitButton = findViewById(R.id.quitButton);
 
         startButton.setOnClickListener(view -> {
-            // Create an Intent to start the GameActivity
-            Intent game_config = new Intent(MainActivity.this, GameConfigurationActivity.class);//replace with nme of class
-
-            // Optionally, you can add extra data to the intent
-            // intent.putExtra("key", "value");
-
-            // Start the GameActivity
-            startActivity(game_config);
+            Intent gameConfig = new Intent(MainActivity.this, GameConfigurationActivity.class);
+            startActivity(gameConfig);
             finish();
         });
-
         quitButton.setOnClickListener(view -> {
-            // Finish the current activity to simulate quitting the game
             finish();
             System.exit(0);
         });
-
-
     }
-
-
 }
