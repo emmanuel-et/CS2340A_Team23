db813f070d77cc0620dda3af05846c0ff4d8c9b2
AustinnAI <aaidaewor@icloud.com>
2023-09-19 19:13:02 -0400

    Fixed health display and added end button

--- app/src/main/java/com/example/cs2340a_team23/GameActivity.java
+++ app/src/main/java/com/example/cs2340a_team23/GameActivity.java
@@ -16,7 +16,9 @@ public class GameActivity extends AppCompatActivity {
         Button endButton = findViewById(R.id.endButton);
 
         endButton.setOnClickListener(view -> {
-//            Intent gameConfig = new Intent(GameActivity.this, )
+            Intent gameConfig = new Intent(GameActivity.this, EndActivity.class);
+            startActivity(gameConfig);
+            finish();
         });
     }
 }
