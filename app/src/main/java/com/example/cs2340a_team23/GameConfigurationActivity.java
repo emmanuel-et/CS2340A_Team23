6c6075d21b69b3c70e33db334dd49c9b9c6cd102
Smashthehedgehog <mikeani482@gmail.com>
2023-09-19 15:28:58 -0400

    added a button and text input on xml

--- app/src/main/java/com/example/cs2340a_team23/GameConfigurationActivity.java
+++ app/src/main/java/com/example/cs2340a_team23/GameConfigurationActivity.java
@@ -1,11 +1,21 @@
 package com.example.cs2340a_team23;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
 public class GameConfigurationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_configuration);
+
+        Button playButton = findViewById(R.id.playButton);
+
+        playButton.setOnClickListener(view -> {
+            Intent gamePlay = new Intent(GameConfigurationActivity.this, GameActivity.class);
+            startActivity(gamePlay);
+            finish();
+        });
     }
 }
