a398d3b55c1d0b13862ac6a3a40c4289eeab7be7
Smashthehedgehog <mikeani482@gmail.com>
2023-10-06 11:59:19 -0400

    Update EndActivity.java

--- app/src/main/java/com/example/cs2340a_team23/view/EndActivity.java
+++ app/src/main/java/com/example/cs2340a_team23/view/EndActivity.java
@@ -1,7 +1,10 @@
 package com.example.cs2340a_team23.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.cs2340a_team23.R;
 
@@ -10,5 +13,13 @@ public class EndActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end);
+
+        Button restartButton = findViewById(R.id.restartButton);
+        restartButton.setOnClickListener(view -> {
+//            TODO: Call player reset method
+            Intent gameConfig = new Intent(EndActivity.this, GameConfigurationActivity.class);
+            startActivity(gameConfig);
+            finish();
+        });
     }
 }
