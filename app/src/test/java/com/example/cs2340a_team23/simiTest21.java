f70ff4560e867efd92a56bf9b78c2152ea07a366
simisola oshikanlu <immanuella2004@gmail.com>
2023-10-06 18:03:54 -0400

    eidt

--- app/src/test/java/com/example/cs2340a_team23/simiTest21.java
+++ app/src/test/java/com/example/cs2340a_team23/simiTest21.java
@@ -1,2 +1,30 @@
-package com.example.cs2340a_team23;public class simiTest21 {
+package com.example.cs2340a_team23;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+
+import com.example.cs2340a_team23.model.Player;
+
+
+public class simiTest21 {
+
+
+    private Player player;
+
+
+    @Before
+    public void setUp() {
+        player = Player.getPlayer();
+    }
+    @Test
+    public void healthIsRight() {
+        assertEquals(100, player.getHealth());
+    }
+
+
 }
