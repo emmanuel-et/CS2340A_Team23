51af292e1a06a97f33ee64b51c6a2bb6dd95791e
Smashthehedgehog <mikeani482@gmail.com>
2023-10-10 00:08:15 -0400

    added Tiles with small problem

---
+++ app/src/main/java/com/example/cs2340a_team23/map/MapLayout.java
@@ -0,0 +1,18 @@
+package com.example.cs2340a_team23.map;
+
+public class MapLayout {
+    public static final int TILE_WIDTH = 16;
+    public static final int TILE_HEIGHT = 16;
+    public static final int ROW_TILES = 30;
+    public static final int COLUMN_TILES = 30;
+
+    private int[][] layout;
+
+    public MapLayout(){
+        initializeLayout();
+    }
+
+    private void initializeLayout() {
+        return;
+    }
+}
