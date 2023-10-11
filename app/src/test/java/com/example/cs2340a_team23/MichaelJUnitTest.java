9054e8e7383c2239e9c9f61912fbc2f1625c8dc0
MichaelJafojo <michaeljafojo@Michaels-MacBook-Air-8.local>
2023-10-10 23:27:22 -0400

    score UI

---
+++ app/src/test/java/com/example/cs2340a_team23/MichaelJUnitTest.java
@@ -0,0 +1,47 @@
+package com.example.cs2340a_team23;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import com.example.cs2340a_team23.model.GameState;
+import com.example.cs2340a_team23.model.Player;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class MichaelJUnitTest {
+
+    private GameState gameState;
+
+    public void setUp() {
+        gameState = GameState.getGameState();
+    }
+    private Player player;
+
+    public void setUp2() {
+        player = Player.getPlayer();
+    }
+
+
+    @Test
+    public void difficultyTest() {
+        assertEquals("hard", gameState.getDifficulty());
+    }
+
+    public void scoreTest() {
+        gameState.setScore(1000);
+        assertEquals(1000, gameState.getScore());
+
+    }
+    public void playerTest() {
+        player.setPlayerName("Mikeyj");
+        assertEquals("Mikeyj", player.getPlayerName());
+
+    }
+
+
+
+}
