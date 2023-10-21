f4ac074a69fad283eac1118b7b7b68c455cc4da5
simisola oshikanlu <immanuella2004@gmail.com>
2023-10-21 11:22:19 -0400

    made test cases

--- app/src/test/java/com/example/cs2340a_team23/MikeAUnitTests.java
+++ app/src/test/java/com/example/cs2340a_team23/MikeAUnitTests.java
@@ -6,14 +6,19 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import com.example.cs2340a_team23.model.Player;
+import com.example.cs2340a_team23.model.Run;
+import com.example.cs2340a_team23.model.Walk;
 
 public class MikeAUnitTests {
-
+    private Run run;
+    private Walk walk;
     private Player player;
 
     @Before
     public void setUp() {
+        run = new Run();
         player = Player.getPlayer();
+        walk = new Walk();
     }
     @Test
     public void setNewHealthIsCorrect() {
@@ -25,5 +30,27 @@ public class MikeAUnitTests {
     public void setPlayerNameWithSymbolsIsCorrect() {
         player.setPlayerName("???");
         assertEquals("???", player.getPlayerName());
+    }
+    @Test
+    public void testMoveUp() {
+        float[] initialPosition = {100.0f, 100.0f};
+        int screenWidth = 800;
+        int screenHeight = 600;
+        int spriteWidth = 50;
+        int spriteHeight = 50;
+        float[] newPosition = walk.move(initialPosition[0], initialPosition[1], "up", screenWidth, screenHeight, spriteWidth, spriteHeight);
+        float[] expectedPosition = {100.0f, 50.0f};
+        assertArrayEquals(expectedPosition, newPosition, 0.01F);
+    }
+    @Test
+    public void testMoveLeft() {
+        float[] initialPosition = {100.0f, 100.0f};
+        int screenWidth = 800;
+        int screenHeight = 600;
+        int spriteWidth = 50;
+        int spriteHeight = 50;
+        float[] newPosition = run.move(initialPosition[0], initialPosition[1], "left", screenWidth, screenHeight, spriteWidth, spriteHeight);
+        float[] expectedPosition = {0.0f, 100.0f};
+        assertArrayEquals(expectedPosition, newPosition, 0.01F);
     }
 }
--- app/src/test/java/com/example/cs2340a_team23/ShawnaUnitTest.java
+++ app/src/test/java/com/example/cs2340a_team23/ShawnaUnitTest.java
@@ -5,15 +5,24 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.example.cs2340a_team23.model.Leaderboard;
+import com.example.cs2340a_team23.model.LeaderboardEntry;
 import com.example.cs2340a_team23.model.Player;
+import com.example.cs2340a_team23.model.Walk;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
 public class ShawnaUnitTest {
-
+    private Leaderboard leaderboard;
+    private Walk walk;
     private Player player;
 
     @Before
     public void setUp() {
         player = Player.getPlayer();
+        walk = new Walk();
     }
     @Test
     public void checkInitializePlayer() {
@@ -27,5 +36,27 @@ public class ShawnaUnitTest {
     public void checkPlayerSprite() {
         player.setSprite("megaman");
         assertEquals("megaman", player.getSprite());
+    }
+    @Test
+    public void testMoveDown() {
+        float[] initialPosition = {100.0f, 100.0f};
+        int screenWidth = 800;
+        int screenHeight = 600;
+        int spriteWidth = 50;
+        int spriteHeight = 50;
+        float[] newPosition = walk.move(initialPosition[0], initialPosition[1], "down", screenWidth, screenHeight, spriteWidth, spriteHeight);
+        float[] expectedPosition = {100.0f, 150.0f};
+        assertArrayEquals(expectedPosition, newPosition, 0.01F);
+    }
+    @Test
+    public void testMoveLeft() {
+        float[] initialPosition = {100.0f, 100.0f};
+        int screenWidth = 800;
+        int screenHeight = 600;
+        int spriteWidth = 50;
+        int spriteHeight = 50;
+        float[] newPosition = walk.move(initialPosition[0], initialPosition[1], "left", screenWidth, screenHeight, spriteWidth, spriteHeight);
+        float[] expectedPosition = {50.0f, 100.0f};
+        assertArrayEquals(expectedPosition, newPosition, 0.01F);
     }
 }
