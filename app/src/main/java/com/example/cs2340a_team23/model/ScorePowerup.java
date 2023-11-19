b818a51c74051ce41ee0d2b4f77183807142f0a3
skalladan3 <you@example.com>
2023-11-19 14:42:57 -0500

    changed file name under drawable to match

--- app/src/main/java/com/example/cs2340a_team23/model/ScorePowerup.java
+++ app/src/main/java/com/example/cs2340a_team23/model/ScorePowerup.java
@@ -25,7 +25,7 @@ public class ScorePowerup extends Powerup {
     @Override
     public void createSpriteView(Context context) {
         this.spriteView = new ImageView(context);
-        int resourceId = context.getResources().getIdentifier("scorekeeper",
+        int resourceId = context.getResources().getIdentifier("scorepowerup",
                 "drawable", context.getPackageName());
         spriteView.setImageResource(resourceId);
         this.spriteView.setX(this.posX);
