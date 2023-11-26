c2ef179d485dd4b6ad8c9161bfb2659920b0dd03
AustinnAI <aaidaewor@icloud.com>
2023-11-26 17:37:16 -0500

    Added Sword class

---
+++ app/src/main/java/com/example/cs2340a_team23/model/Sword.java
@@ -0,0 +1,12 @@
+package com.example.cs2340a_team23.model;
+
+public class Sword extends Weapon {
+    private int range;
+
+    public Sword() {
+        this.range = 2;
+    }
+    public int getRange() {
+        return range;
+    }
+}
