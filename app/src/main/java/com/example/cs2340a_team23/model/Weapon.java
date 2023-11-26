f2bedc8eb464b8bf8a70fde71f720fb45bd1e296
AustinnAI <aaidaewor@icloud.com>
2023-11-26 17:30:46 -0500

    Defined attack method in Weapon and removed damage

--- app/src/main/java/com/example/cs2340a_team23/model/Weapon.java
+++ app/src/main/java/com/example/cs2340a_team23/model/Weapon.java
@@ -1,18 +1,17 @@
 package com.example.cs2340a_team23.model;
 
 public abstract class Weapon {
-    private int damage;
     private int range;
-
-    public int getDamage() {
-        return damage;
-    }
 
     public int getRange() {
         return range;
     }
 
-    public void attack(Enemy enemy) {
+    public void attack(Enemy enemy, Weapon weapon) {
+        double distance = Math.sqrt(Math.pow(Player.getPlayer().getPlayerX() - enemy.getEnemyX(), 2) + Math.pow(Player.getPlayer().getPlayerY() - enemy.getEnemyY(), 2));
 
+        if (distance <= range) {
+            enemy.destroy();
+        }
     }
 }
