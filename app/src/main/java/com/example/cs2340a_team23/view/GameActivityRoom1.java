891d7b97610f42a470898e7eb89eb756513bdec5
AustinnAI <aaidaewor@icloud.com>
2023-11-26 21:37:47 -0500

    Removed unnecessary method from room 1

--- app/src/main/java/com/example/cs2340a_team23/view/GameActivityRoom1.java
+++ app/src/main/java/com/example/cs2340a_team23/view/GameActivityRoom1.java
@@ -310,29 +310,4 @@ public class GameActivityRoom1 extends AppCompatActivity {
         }
     }
 
-    private Enemy checkAttackDistance() {
-        float playerWidth = 30;
-        float playerHeight = 40;
-        float enemyWidth = 32;
-        float enemyHeight = 32;
-        float playerCenterX = player.getPlayerX() + playerWidth / 2;
-        float playerCenterY = player.getPlayerY() + playerHeight / 2;
-        for (Enemy enemy : enemies) {
-            float enemyCenterX = enemy.getEnemyX() + enemyWidth / 2;
-            float enemyCenterY = enemy.getEnemyY() + enemyHeight / 2;
-
-            // Calculate the distance between player and enemy centers
-            double distance = Math.sqrt(Math.pow(playerCenterX - enemyCenterX, 2) + Math.pow(playerCenterY - enemyCenterY, 2));
-
-            if (distance <= player.getWeapon().getRange()) {
-                return enemy;
-            }
-        }
-        return null;
-    }
-
-
-
-
-
 }
